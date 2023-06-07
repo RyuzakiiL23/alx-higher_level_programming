2d5d07d5480be3ecfc92fe928d265d2e2f94c2c2
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-07 01:58:05 +0100

    Nodes

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,10 +5,10 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *tmp = *head;
-	
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = number;
 
 	if (tmp == NULL || tmp->n >= number)
@@ -18,12 +18,12 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 
-	while(tmp->next != NULL && tmp->next->n < number)
+	while (tmp->next != NULL && tmp->next->n < number)
 	{
 		tmp = tmp->next;
 	}
 	new->next = tmp->next;
 	tmp->next = new;
 
-	return(new);
+	return (new);
 }
