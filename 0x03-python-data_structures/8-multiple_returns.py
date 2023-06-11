b8141b11c71814c1aa071e02d74071db5e008287
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-11 12:09:25 +0100

    multiple_returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        length = 0
+        first = None 
+        
+    else:
+        length = len(sentence)
+        first = sentence[0]
+
+    tpl = (length, first)
+
+    return tpl
