c857ebebbd391cd6d5fe478c7973b76caaa780cc
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-11 12:17:35 +0100

    multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         length = 0
-        first = None 
-        
+        first = None
+
     else:
         length = len(sentence)
         first = sentence[0]
