df365c118b549242a0f5e8c1641ff7ea662b3e70
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-26 19:04:34 +0100

    ZeroError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = float(a / b)
+        result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result:{}".format(result))
+        if isinstance(result, int):
+            result = float(result)
+            print("Inside result:{}".format(result))
+        else:
+            print("Inside result:{}".format(result))
     return result
