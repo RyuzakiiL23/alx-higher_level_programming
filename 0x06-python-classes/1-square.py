a0ac928a20edfe56ac40ec8fee1b7928ae80c00e
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-27 18:18:51 +0100

    InitializeSquare

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent Square."""
+
+    def __init__(self, size):
+        """Initialize The Square."""
+        self.__size = size
