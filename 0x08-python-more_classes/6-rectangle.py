21c593a65eef284af59e9117e1d8fe1e748f4238
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-07-03 18:23:23 -0400

    Bye

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -68,5 +68,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message when an instance of Rectangle is deleted"""
-        type(self).number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
