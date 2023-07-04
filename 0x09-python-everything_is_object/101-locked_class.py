b1d58d4287963b75b864137b9a6f61d6a9d745fa
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-07-04 23:23:05 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-"""Defines a locked class."""
-
-
 class LockedClass:
-    def __setattr__(self, name, value):
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
