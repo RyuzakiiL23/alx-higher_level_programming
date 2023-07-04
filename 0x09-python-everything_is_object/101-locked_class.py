0588783dd42cb7a61462225bfaa36e630881cbe3
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-07-04 23:14:54 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,6 +4,4 @@
 
 class LockedClass:
     def __setattr__(self, name, value):
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError("Cannot create new instance attributes")
         super().__setattr__(name, value)
