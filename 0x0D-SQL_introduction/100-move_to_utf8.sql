9826f2fe8e756a183ebe24d8eaed08dd738b3eae
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-08-15 17:10:22 +0100

    optional

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,3 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
-USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Script that converts hbtn_0c_0 database to UTF8
+USE `hbtn_0c_0` ALTER TABLE `first_table` CONVERT TO CHARACTER
+SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,3 @@
--- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+-- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
