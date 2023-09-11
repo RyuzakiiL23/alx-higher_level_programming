a5a5010727596fa5125de94d9cbb4e2f5d12e4ce
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-11 18:31:34 +0100

    tast_6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const i of list) {
   console.log(i);
