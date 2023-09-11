2a0394c5410af2a6f747d1579634309b64197a31
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-11 18:10:33 +0100

    tast_5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const args = process.argv.splice(2);
 if (isNaN(parseInt(args[0]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(args[0]));
+  console.log(`My number: ${parseInt(args[0])}`);
 }
