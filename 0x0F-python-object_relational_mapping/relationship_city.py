e4af897a8909264482a4d898eeeb0a272efb0a0d
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-18 00:58:45 +0100

    task_17

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a City
-"""
+"""Class definition of a City"""
 from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """
-    Class that defines each city
-    """
+    """Class definition city"""
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains State class and Base, an instance of declarative_base()
-"""
+"""Definition base and state"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -11,9 +9,7 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """
-    Class with id and name attributes of each state
-    """
+    """Define states"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
