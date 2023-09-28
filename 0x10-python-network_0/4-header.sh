979072c90be6a38f9538d02d5edc7146ae1ee914
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-28 12:37:14 +0100

    tast_4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a GET request to the URL, and displays the body the response
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -sH "X-School-User-Id: 98" "${1}"
