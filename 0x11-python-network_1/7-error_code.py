3e81c08c668e3a8ba38bcf676dc39b7603c9bc8c
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-29 14:44:06 +0100

    task_7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)."""
+
+import requests
+import sys
+if __name__ == "__main__":
+    try:
+        with requests.get(sys.argv[1]) as res:
+            print(res.text)
+    except requests.exceptions.RequestException as err:
+        print('Error code:', err)
