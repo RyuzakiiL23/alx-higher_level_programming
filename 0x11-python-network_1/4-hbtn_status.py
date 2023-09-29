6c78003c3869a3ecfc48311ca6c53a30cace939b
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-29 14:10:47 +0100

    task_4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
+import requests
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
-        r = f.read()
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(r)))
-    print('\t- content: {}'.format(r))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
