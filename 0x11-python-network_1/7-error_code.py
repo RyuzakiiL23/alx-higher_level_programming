f9a708990c1f44db1bee8ac5825ba2528f2c103e
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-09-29 14:50:26 +0100

    task_7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,7 @@ import sys
 if __name__ == "__main__":
     try:
         with requests.get(sys.argv[1]) as res:
+            res.raise_for_status()
             print(res.text)
     except requests.exceptions.RequestException as err:
-        print('Error code:', err)
+        print('Error code:', str(err))
