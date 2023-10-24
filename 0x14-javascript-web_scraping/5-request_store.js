c9ab7ac40b747770cea4e5203efd44352739ab30
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-24 19:35:46 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,10 +6,10 @@ const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
 
-request.get(url, (error, response, body) => {
-fs.writeFile(file, body, { encoding: 'utf8', flag: 'w' }, (err) => {
-  if (err) {
-    console.error(err);
-  }
-})
+request.get(url, (response, body) => {
+  fs.writeFile(file, body, { encoding: 'utf8', flag: 'w' }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
